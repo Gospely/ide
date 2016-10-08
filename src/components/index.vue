a2f3680a365df76981189936d60080e530ccd70c
leinue <597055914@qq.com>
2016-10-08 20:24:31 +0800

    update

--- src/components/index.vue
+++ src/components/index.vue
@@ -14,7 +14,7 @@
 
               </ui-tab>
               <ui-tab header="文件">
-
+                <ul id="treeDemo" class="ztree"></ul>
               </ui-tab>
           </ui-tabs>
         </div>
@@ -59,6 +59,104 @@ import store from '../vuex.js';
 import forms from './forms/form.vue';
 import terminal from './terminal.vue';
 
+    var setting = {
+      edit: {
+        enable: true,
+        showRemoveBtn: true,
+        showRenameBtn: true
+      },
+
+      data: {
+        simpleData: {
+          enable: true
+        }
+      },
+
+      callback: {
+        onExpand: onExpand,
+        onClick: onClick
+      },
+
+      view: {
+        dblClickExpand: false,
+        addHoverDom: addHoverDom,
+        removeHoverDom: removeHoverDom
+      }
+    };
+
+    var zNodes =[
+      { id:1, pId:0, name:"随意拖拽 1", open:true},
+      { id:11, pId:1, name:"随意拖拽 1-1"},
+      { id:12, pId:1, name:"随意拖拽 1-2"},
+      { id:121, pId:12, name:"随意拖拽 1-2-1"},
+      { id:122, pId:12, name:"随意拖拽 1-2-2"},
+      { id:123, pId:12, name:"随意拖拽 1-2-3"},
+      { id:13, pId:1, name:"禁止拖拽 1-3", open:true, drag:false},
+      { id:131, pId:13, name:"禁止拖拽 1-3-1", drag:false},
+      { id:132, pId:13, name:"禁止拖拽 1-3-2", drag:false},
+      { id:132, pId:13, name:"禁止拖拽 1-3-3", drag:false},
+      { id:2, pId:0, name:"禁止子节点移走 2", open:true, childOuter:false},
+      { id:21, pId:2, name:"我不想成为父节点 2-1", dropInner:false},
+      { id:22, pId:2, name:"我不要成为根节点 2-2", dropRoot:false},
+      { id:23, pId:2, name:"拖拽试试看 2-3"},
+      { id:3, pId:0, name:"禁止子节点排序/增加 3", open:true, childOrder:false, dropInner:false},
+      { id:31, pId:3, name:"随意拖拽 3-1"},
+      { id:32, pId:3, name:"随意拖拽 3-2"},
+      { id:33, pId:3, name:"随意拖拽 3-3"}
+    ];
+
+    function onClick(e,treeId, treeNode) {
+      var zTree = $.fn.zTree.getZTreeObj("treeDemo");
+      zTree.expandNode(treeNode);
+    }
+
+    var newCount = 1;
+    function addHoverDom(treeId, treeNode) {
+      var sObj = $("#" + treeNode.tId + "_span");
+      if (treeNode.editNameFlag || $("#addBtn_"+treeNode.tId).length>0) return;
+      var addStr = "<span class='button add' id='addBtn_" + treeNode.tId
+        + "' title='add node' onfocus='this.blur();'></span>";
+      sObj.after(addStr);
+      var btn = $("#addBtn_"+treeNode.tId);
+      if (btn) btn.bind("click", function(){
+        var zTree = $.fn.zTree.getZTreeObj("treeDemo");
+        zTree.addNodes(treeNode, {id:(100 + newCount), pId:treeNode.id, name:"new node" + (newCount++)});
+        return false;
+      });
+    };
+
+    function removeHoverDom(treeId, treeNode) {
+      $("#addBtn_"+treeNode.tId).unbind().remove();
+    };
+
+    function onExpand(event, treeId, treeNode) {
+      if (treeNode === autoExpandNode) {
+        className = (className === "dark" ? "":"dark");
+        showLog("[ "+getTime()+" onExpand ]&nbsp;&nbsp;&nbsp;&nbsp;" + treeNode.name);
+      }
+    }
+
+    function showLog(str) {
+      if (!log) log = $("#log");
+      log.append("<li class='"+className+"'>"+str+"</li>");
+      if(log.children("li").length > 8) {
+        log.get(0).removeChild(log.children("li")[0]);
+      }
+    }
+    function getTime() {
+      var now= new Date(),
+      h=now.getHours(),
+      m=now.getMinutes(),
+      s=now.getSeconds(),
+      ms=now.getMilliseconds();
+      return (h+":"+m+":"+s+ " " +ms);
+    }
+
+    function setTrigger() {
+      var zTree = $.fn.zTree.getZTreeObj("treeDemo");
+      zTree.setting.edit.drag.autoExpandTrigger = $("#callbackTrigger").attr("checked");
+    }
+
 export default {
   data () {
     return {
@@ -86,6 +184,8 @@ export default {
   },
 
   ready() {
+    $.fn.zTree.init($("#treeDemo"), setting, zNodes);
+    $("#callbackTrigger").bind("change", {}, setTrigger);  
   },
 
   components: {
