f29245b3c6005a2681ec4b3fd416404c397b81ca
leinue <597055914@qq.com>
2016-10-19 14:36:44 +0800

    update

--- src/components/index.vue
+++ src/components/index.vue
@@ -10,10 +10,9 @@
                 <controls-panel></controls-panel>
               </ui-tab>
               <ui-tab header="内容">
-                  <div id="tree"></div>
               </ui-tab>
               <ui-tab header="文件">
-                <ul id="file-tree" class="ztree"></ul>
+                <div v-bind:id="fileTreeSelector"></div>
               </ui-tab>
           </ui-tabs>
         </div>
@@ -54,11 +53,6 @@ import store from '../vuex.js';
 import forms from './forms/form.vue';
 import terminal from './terminal.vue';
 
-function setTrigger() {
-  var zTree = $.fn.zTree.getZTreeObj("file-tree");
-  zTree.setting.edit.drag.autoExpandTrigger = $("#callbackTrigger").attr("checked");
-}
-
 export default {
   data () {
     return {
@@ -84,49 +78,55 @@ export default {
         width: '100%'
       },
 
-      treeNode: [],
+      files: [],
       fileTreeSelector: 'file-tree'
     }
   },
 
   ready() {
 
-    var self = this;
-
-    var setting = {
-      edit: {
-        enable: true,
-        showRemoveBtn: true,
-        showRenameBtn: true
-      },
-
-      data: {
-        simpleData: {
-          enable: true
-        }
-      },
-
-      callback: {
-        onExpand: this.$get('onExpand'),
-        onClick: this.$get('treeNodeOnClick')
-      },
-
-      view: {
-        dblClickExpand: false,
-        // addHoverDom: this.$get('addHoverDom'),
-        // removeHoverDom: this.$get('removeHoverDom')
-      }
-    };
-
     this.$nextTick(() => {
 
-      $('#tree')
+      $Model.FSService.ls().then( (res) => {
+
+        var response = res.data,
+            fileTree = response.data;
+
+        if(response.code == 200) {
+
+          this.$set('files', fileTree);
+          this.$get('initFileTree')(); 
+
+        }else {
+          util.alert(response.message);
+        }
+
+      }).catch( (error) => {
+        util.alert('文件树请求失败');
+      });
+
+    });
+
+  },
+
+  components: {
+    controlsPanel,
+    designPanel,
+    forms,
+    attrPanel,
+    terminal
+  },
+
+  methods: {
+
+    initFileTree: function() {
+
+      $('#' + this.fileTreeSelector)
         .jstree({
           'core' : {
             'data' : {
-              'url' : 'http://localhost:8089/fs/list/optional/',
+              'url' : 'http://api.gospely.com/fs/list/optional/',
               'data' : function (node) {
-                console.log('node', node);
                 return { 'id' : node.id == '#' ? '' : node.id };
               }
             },
@@ -237,7 +237,7 @@ export default {
         })
         .on('changed.jstree', function (e, data) {
           if(data && data.selected && data.selected.length) {
-            $.get('http://localhost:8089/fs/list/optional/' + data.selected, function (d) {
+            $.get('http://api.gospely.com/fs/list/optional/' + data.selected, function (d) {
               if(d && typeof d.type !== 'undefined') {
                 $('#data .content').hide();
                 switch(d.type) {
@@ -269,108 +269,12 @@ export default {
                 }
               }
             });
-          }
-          else {
-            $('#data .content').hide();
-            $('#data .default').html('Select a file from the tree.').show();
-          }
-        });
-
-      $Model.FSService.ls().then( (res) => {
-
-        var response = res.data,
-            fileTree = response.data;
-
-        if(response.code == 200) {
-
-          var result = [];
-
-          for (var i = 0; i < response.fields.length; i++) {
-            var field = response.fields[i];
-
-            var node = {
-              id: field.id,
-              name: field.name,
-              open: false,
-              path: field.path,
-              pId: 0
-            };
-
-            result.push(node);
-
-            if(field.sub.length != 0) {
-
-              for (var j = 0; j < field.sub.length; j++) {
-                var sub = field.sub[j];
-
-                node = {
-                  id: sub.id,
-                  name: sub.name,
-                  path: sub.path,
-                  pId: field.id
-                }
-
-                result.push(node);
-
-              };
-
-            }
-
-          };
-
-          this.$set('treeNode', result);
-          $.fn.zTree.init($("#" + this.$get('fileTreeSelector')), setting, this.$get('treeNode'));
-          $("#callbackTrigger").bind("change", {}, setTrigger);  
-
-        }else {
-          util.alert(response.message);
+        } else {
+          $('#data .content').hide();
+          $('#data .default').html('Select a file from the tree.').show();
         }
-
-      }).catch( (error) => {
-        util.alert('文件树请求失败');
       });
 
-    });
-
-  },
-
-  components: {
-    controlsPanel,
-    designPanel,
-    forms,
-    attrPanel,
-    terminal
-  },
-
-  methods: {
-
-    onExpand: function(event, treeId, treeNode) {
-      if (treeNode === autoExpandNode) {
-        className = (className === "dark" ? "":"dark");
-      }
-    },
-
-    treeNodeOnClick: function(e,treeId, treeNode) {
-      var zTree = $.fn.zTree.getZTreeObj(this.fileTreeSelector);
-      zTree.expandNode(treeNode);
-    },
-
-    addHoverDom: function(treeId, treeNode) {
-      var sObj = $("#" + treeNode.tId + "_span");
-      if (treeNode.editNameFlag || $("#addBtn_"+treeNode.tId).length>0) return;
-      var addStr = "<span class='button add' id='addBtn_" + treeNode.tId
-        + "' title='add node' onfocus='this.blur();'></span>";
-      sObj.after(addStr);
-      var btn = $("#addBtn_"+treeNode.tId);
-      if (btn) btn.bind("click", function(){
-        var zTree = $.fn.zTree.getZTreeObj(this.fileTreeSelector);
-        zTree.addNodes(treeNode, {id:(100 + newCount), pId:treeNode.id, name:"new node" + (newCount++)});
-        return false;
-      });
-    },
-
-    removeHoverDom: function(treeId, treeNode) {
-      $("#addBtn_"+treeNode.tId).unbind().remove();
     }
 
   },
@@ -407,34 +311,5 @@ export default {
   #tools .ui-tabs-body {
     margin-top: 1px;
   }
-
-  #data { margin-left:320px; }
-  #data textarea { margin:0; padding:0; height:100%; width:100%; border:0; background:white; display:block; line-height:18px; resize:none; }
-  #data, #code { font: normal normal normal 12px/18px 'Consolas', monospace !important; }
-
-  #tree .folder { background:url('https://www.jstree.com/demo_filebrowser/file_sprite.png') right bottom no-repeat; }
-  #tree .file { background:url('https://www.jstree.com/demo_filebrowser/file_sprite.png') 0 0 no-repeat; }
-  #tree .file-pdf { background-position: -32px 0 }
-  #tree .file-as { background-position: -36px 0 }
-  #tree .file-c { background-position: -72px -0px }
-  #tree .file-iso { background-position: -108px -0px }
-  #tree .file-htm, #tree .file-html, #tree .file-xml, #tree .file-xsl { background-position: -126px -0px }
-  #tree .file-cf { background-position: -162px -0px }
-  #tree .file-cpp { background-position: -216px -0px }
-  #tree .file-cs { background-position: -236px -0px }
-  #tree .file-sql { background-position: -272px -0px }
-  #tree .file-xls, #tree .file-xlsx { background-position: -362px -0px }
-  #tree .file-h { background-position: -488px -0px }
-  #tree .file-crt, #tree .file-pem, #tree .file-cer { background-position: -452px -18px }
-  #tree .file-php { background-position: -108px -18px }
-  #tree .file-jpg, #tree .file-jpeg, #tree .file-png, #tree .file-gif, #tree .file-bmp { background-position: -126px -18px }
-  #tree .file-ppt, #tree .file-pptx { background-position: -144px -18px }
-  #tree .file-rb { background-position: -180px -18px }
-  #tree .file-text, #tree .file-txt, #tree .file-md, #tree .file-log, #tree .file-htaccess { background-position: -254px -18px }
-  #tree .file-doc, #tree .file-docx { background-position: -362px -18px }
-  #tree .file-zip, #tree .file-gz, #tree .file-tar, #tree .file-rar { background-position: -416px -18px }
-  #tree .file-js { background-position: -434px -18px }
-  #tree .file-css { background-position: -144px -0px }
-  #tree .file-fla { background-position: -398px -0px }
 
 </style>
